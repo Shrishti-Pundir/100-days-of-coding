b13ac6b4d466d8d420831cae67430f4db3570564
Shrishti-Pundir <shrishtipundir9758@gmail.com>
2025-09-14 22:12:22 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,10 @@
+//Write a program to input two numbers and display their sum.
+
+#include <stdio.h>
+void main()
+{
+    int a, b;
+    printf("Enter two numbers:\n");
+    scanf("%d %d", &a, &b); 
+    printf("Sum = %d", (a+b));
+}
