b9f97fa2249ce914219525b1e49c68fa7475a96d
Shrishti Pundir <shrishtipundir9758@gmail.com>
2025-09-18 15:36:26 +0530

    Day 25 ques 1
    Day 25 ques 1

---
+++ 49.c
@@ -0,0 +1,22 @@
+/*Write a program to print the following pattern:
+5
+45
+345
+2345
+12345 */
+
+
+#include <stdio.h>
+void main()
+{
+    printf("Pattern:- \n");
+    int s=5;
+    for(int i=5;i>=1;i--)
+    {
+        for(int j=i;j<=s;j++)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+}
