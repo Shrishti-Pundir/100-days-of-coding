0d968862c8aec2308e135f8f245ab9362b56570a
Shrishti Pundir <shrishtipundir9758@gmail.com>
2025-09-18 15:40:34 +0530

    Day 23 ques 2
    Day 23 ques 2

---
+++ 46.c
@@ -0,0 +1,21 @@
+/* Write a program to print the following pattern:
+*****
+*****
+*****
+*****
+***** */
+
+
+#include <stdio.h>
+void main()
+{
+    printf("Pattern:- \n");
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=1;j<=5;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
