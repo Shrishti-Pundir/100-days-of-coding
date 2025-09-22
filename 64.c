851fa2af378f54f0209413cdeff1d9cc2705db18
Shrishti Pundir <shrishtipundir9758@gmail.com>
2025-09-22 23:37:50 +0530

    day 32 ques 2
    day 32 ques 2

--- 64.c
+++ 64.c
@@ -7,8 +7,10 @@ int main()
     long long n;
     int f[10] = {0}, d, i, mx = 0;
     scanf("%lld", &n);
-    if (n < 0) n = -n;
-    if (n == 0) f[0] = 1;
+    if (n < 0) 
+        n = -n;
+    if (n == 0) 
+        f[0] = 1;
     while (n > 0) 
     {
         d = n % 10;
@@ -26,3 +28,4 @@ int main()
     printf("\n");
     return 0;
 }
+
