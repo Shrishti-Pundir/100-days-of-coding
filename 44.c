f8045d2db3f1f689c16a144fb3d40351aa417d2a
Shrishti Pundir <shrishtipundir9758@gmail.com>
2025-10-01 11:10:02 +0530

    day 22 ques 2
    day 22 ques 2

---
+++ 44.c
@@ -0,0 +1,16 @@
+//Write a program to find the sum of the series: 1 + 3/4 + 5/6 + 7/8 + … up to n terms.
+
+
+#include <stdio.h>
+void main()
+{
+    int n;
+    double sum = 1.0;
+    printf("Enter the no. of terms: \n");
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++)
+    {
+        sum += (double)(2*i - 1) / (2*i);
+    }
+    printf("Sum of series up to %d terms = %.6f\n", n, sum);
+}
