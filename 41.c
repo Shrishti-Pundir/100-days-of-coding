494f582c1feb17916e99d2f71e59eb218233d88c
Shrishti Pundir <shrishtipundir9758@gmail.com>
2025-10-06 21:08:24 +0530

    day 21 ques 1
    day 21 ques 1

---
+++ 41.c
@@ -0,0 +1,23 @@
+//Write a program to swap the first and last digit of a number.
+
+
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+    int num, firstDigit, lastDigit, digits, swapNum;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    lastDigit = num % 10;
+    digits = (int)log10(num);
+    firstDigit = (int)(num / pow(10, digits));
+    if (firstDigit == lastDigit)
+    {
+        printf("Number after swapping first and last digit is: %d\n", num);
+    }
+    else
+    {
+        swapNum = (lastDigit * pow(10, digits)) + (num % (int)pow(10, digits) - lastDigit) + firstDigit;
+        printf("Swapped numbers = %d\n", swapNum);
+    }
+}
